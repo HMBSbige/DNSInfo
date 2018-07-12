9daa1aa964a8c826e82096b2f7e1958f2f0f5e48
HMBSbige <admin@bige0.com>
2018-07-12 19:23:42 +0800

    1.0

--- DNSInfo/DNSInfo.Designer.cs
+++ DNSInfo/DNSInfo.Designer.cs
@@ -28,20 +28,149 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.statusStrip1.SuspendLayout();
 			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Dock = System.Windows.Forms.DockStyle.Top;
+			this.textBox1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.Location = new System.Drawing.Point(0, 0);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ReadOnly = true;
+			this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.textBox1.Size = new System.Drawing.Size(561, 238);
+			this.textBox1.TabIndex = 0;
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 302);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(561, 22);
+			this.statusStrip1.TabIndex = 1;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// toolStripStatusLabel1
+			// 
+			this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+			this.toolStripStatusLabel1.Size = new System.Drawing.Size(80, 17);
+			this.toolStripStatusLabel1.Text = "服务器响应：";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(95, 243);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 21);
+			this.textBox2.TabIndex = 2;
+			this.textBox2.Text = "1.1.1.1";
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(201, 248);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(72, 16);
+			this.checkBox1.TabIndex = 3;
+			this.checkBox1.Text = "缓存结果";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(14, 270);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(259, 21);
+			this.textBox3.TabIndex = 4;
+			this.textBox3.Text = "www.google.com";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(472, 268);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 5;
+			this.button1.Text = "查询";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "IPv4 host address (A)",
+            "Name server (NS)",
+            "Canonical name (CNAME)",
+            "Start of authority (SOA)",
+            "Domain name pointer (PTR)",
+            "Domain name pointer (HINFO)",
+            "Mail exchange (MX)",
+            "Text entries (TXT)",
+            "IPv6 host address (AAAA)",
+            "Any record (ANY)"});
+			this.comboBox1.Location = new System.Drawing.Point(279, 270);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(187, 20);
+			this.comboBox1.TabIndex = 6;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 248);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(77, 12);
+			this.label1.TabIndex = 7;
+			this.label1.Text = "DNS 服务器：";
 			// 
 			// DNSInfo
 			// 
+			this.AcceptButton = this.button1;
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(561, 324);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.statusStrip1);
+			this.Controls.Add(this.textBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
 			this.Name = "DNSInfo";
+			this.ShowIcon = false;
 			this.Text = "DNSInfo";
+			this.Resize += new System.EventHandler(this.DNSInfo_Resize);
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label1;
 	}
 }
 
--- DNSInfo/DNSInfo.cs
+++ DNSInfo/DNSInfo.cs
@@ -1,4 +1,9 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LumiSoft.Net.DNS;
+using LumiSoft.Net.DNS.Client;
 
 namespace DNSInfo
 {
@@ -7,6 +12,232 @@ namespace DNSInfo
 		public DNSInfo()
 		{
 			InitializeComponent();
+			comboBox1.SelectedIndex = 0;
+		}
+
+		private delegate void VoidMethodDelegate();
+
+		private const string Statushead = @"服务器响应：";
+
+		private void EnableAll()
+		{
+			button1.Enabled = true;
+			comboBox1.Enabled = true;
+			textBox2.Enabled = true;
+			textBox3.Enabled = true;
+			checkBox1.Enabled = true;
+		}
+
+		private void DisableAll()
+		{
+			button1.Enabled = false;
+			comboBox1.Enabled = false;
+			textBox2.Enabled = false;
+			textBox3.Enabled = false;
+			checkBox1.Enabled = false;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				DisableAll();
+				textBox1.Text = string.Empty;
+				var querystr = textBox3.Text;
+				DNS_QType type;
+				if (comboBox1.SelectedIndex == 0)
+				{
+					type = DNS_QType.A;
+				}
+				else if (comboBox1.SelectedIndex == 1)
+				{
+					type = DNS_QType.NS;
+				}
+				else if (comboBox1.SelectedIndex == 2)
+				{
+					type = DNS_QType.CNAME;
+				}
+				else if (comboBox1.SelectedIndex == 3)
+				{
+					type = DNS_QType.SOA;
+				}
+				else if (comboBox1.SelectedIndex == 4)
+				{
+					type = DNS_QType.PTR;
+				}
+				else if (comboBox1.SelectedIndex == 5)
+				{
+					type = DNS_QType.HINFO;
+				}
+				else if (comboBox1.SelectedIndex == 6)
+				{
+					type = DNS_QType.MX;
+				}
+				else if (comboBox1.SelectedIndex == 7)
+				{
+					type = DNS_QType.TXT;
+				}
+				else if (comboBox1.SelectedIndex == 8)
+				{
+					type = DNS_QType.AAAA;
+				}
+				else if (comboBox1.SelectedIndex == 9)
+				{
+					type = DNS_QType.ANY;
+				}
+				else
+				{
+					throw new Exception(@"无效查询类型");
+				}
+
+				Dns_Client.DnsServers = textBox2.Text.Split(new[] { ',', '，' }, StringSplitOptions.RemoveEmptyEntries);
+				Dns_Client.UseDnsCache = checkBox1.Checked;
+
+				DnsServerResponse reponse = null;
+				double latency = 0;
+
+				var t = new Task(() =>
+				{
+					using (var dns = new Dns_Client())
+					{
+						var stopwatch = new Stopwatch();
+						stopwatch.Start();
+						reponse = dns.Query(querystr, type);
+						stopwatch.Stop();
+						latency = Math.Round(stopwatch.ElapsedTicks / (double)Stopwatch.Frequency, 2);
+					}
+				});
+				t.Start();
+				t.ContinueWith(task =>
+				{
+					BeginInvoke(new VoidMethodDelegate(() =>
+					{
+						if (reponse == null)
+						{
+							toolStripStatusLabel1.Text = Statushead + @"DNS 服务器未响应!";
+							EnableAll();
+							return;
+						}
+						if (!reponse.ConnectionOk)
+						{
+							toolStripStatusLabel1.Text = Statushead + @"连接到 DNS 服务器失败!";
+							EnableAll();
+							return;
+						}
+						toolStripStatusLabel1.Text = $@"{Statushead}{reponse.ResponseCode}({latency}ms)";
+
+						var text = string.Empty;
+
+						var tempStr = Records_To_String(reponse.Answers);
+						if (!string.IsNullOrWhiteSpace(tempStr))
+						{
+							text += @"非权威应答:" + Environment.NewLine;
+							text += tempStr;
+						}
+
+						tempStr = Records_To_String(reponse.AuthoritiveAnswers);
+						if (!string.IsNullOrWhiteSpace(tempStr))
+						{
+							text += @"权威应答:" + Environment.NewLine;
+							text += tempStr;
+						}
+
+						tempStr = Records_To_String(reponse.AdditionalAnswers);
+						if (!string.IsNullOrWhiteSpace(tempStr))
+						{
+							text += @"额外应答:" + Environment.NewLine;
+							text += tempStr;
+						}
+
+						textBox1.Text = text;
+						EnableAll();
+					}));
+				});
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(this, @"错误：" + ex.Message, @"错误：", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				EnableAll();
+			}
+		}
+
+		private string Records_To_String(DNS_rr[] records)
+		{
+			var text = string.Empty;
+
+			for (var i = 0; i < records.Length; ++i)
+			{
+				var record = records[i];
+
+				text += @"Record " + (i + 1) + @": Name=""" + record.Name + @""", Type=" + record.RecordType + @", TTL=" + record.TTL + Environment.NewLine;
+
+				if (record.RecordType == DNS_QType.A)
+				{
+					var aRec = (DNS_rr_A)record;
+
+					text += @"IP: " + aRec.IP + Environment.NewLine;
+				}
+				else if (record.RecordType == DNS_QType.NS)
+				{
+					var nsRec = (DNS_rr_NS)record;
+
+					text += @"名称服务器: " + nsRec.NameServer + Environment.NewLine;
+				}
+				else if (record.RecordType == DNS_QType.CNAME)
+				{
+					var cnameRec = (DNS_rr_CNAME)record;
+
+					text += @"别名: " + cnameRec.Alias + Environment.NewLine;
+				}
+				else if (record.RecordType == DNS_QType.SOA)
+				{
+					var soaRec = (DNS_rr_SOA)record;
+
+					text += @"域名服务器: " + soaRec.NameServer + Environment.NewLine;
+					text += @"管理员邮箱: " + soaRec.AdminEmail + Environment.NewLine;
+				}
+				else if (record.RecordType == DNS_QType.PTR)
+				{
+					var ptrRec = (DNS_rr_PTR)record;
+
+					text += @"域名: " + ptrRec.DomainName + Environment.NewLine;
+				}
+				else if (record.RecordType == DNS_QType.HINFO)
+				{
+					var hinfoRec = (DNS_rr_HINFO)record;
+
+					text += @"操作系统: " + hinfoRec.OS + Environment.NewLine;
+					text += @"CPU: " + hinfoRec.CPU + Environment.NewLine;
+				}
+				else if (record.RecordType == DNS_QType.MX)
+				{
+					var mxRec = (DNS_rr_MX)record;
+
+					text += @"MX优先级: " + mxRec.Preference + Environment.NewLine;
+					text += @"邮箱域名: " + mxRec.Host + Environment.NewLine;
+				}
+				else if (record.RecordType == DNS_QType.TXT)
+				{
+					var txtRec = (DNS_rr_TXT)record;
+
+					text += @"文本: " + txtRec.Text + Environment.NewLine;
+				}
+				else if (record.RecordType == DNS_QType.AAAA)
+				{
+					var aRec = (DNS_rr_AAAA)record;
+
+					text += @"IP: " + aRec.IP + Environment.NewLine;
+				}
+
+				text += Environment.NewLine;
+			}
+
+			return text;
+		}
+
+		private void DNSInfo_Resize(object sender, EventArgs e)
+		{
+
 		}
 	}
 }
