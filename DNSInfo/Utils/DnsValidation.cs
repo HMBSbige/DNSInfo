d8263f410f8ef6e7e8d987dd8ce97ec35164f130
Bruce Wayne <admin@bige0.com>
2019-04-25 23:43:53 +0800

    Add DnsValidation

---
+++ DNSInfo/Utils/DnsValidation.cs
@@ -0,0 +1,85 @@
+﻿using ARSoft.Tools.Net;
+using ARSoft.Tools.Net.Dns;
+using System.Net;
+
+namespace DNSInfo.Utils
+{
+	public static class DnsValidation
+	{
+		public static bool IsSupportDnsSec(IPEndPoint dns)
+		{
+			var dnsClient = new DnsClient(dns.Address, 10000, dns.Port);
+
+			var options = new DnsQueryOptions
+			{
+				IsRecursionDesired = true,
+				IsCheckingDisabled = false,
+				IsEDnsEnabled = true,
+				IsDnsSecOk = true,
+			};
+
+			var dnsMessage = dnsClient.Resolve(DomainName.Parse(@"pir.org"), RecordType.A, RecordClass.INet, options);
+
+			if (dnsMessage == null || dnsMessage.ReturnCode != ReturnCode.NoError && dnsMessage.ReturnCode != ReturnCode.NxDomain)
+			{
+				return false;
+			}
+			return dnsMessage.IsAuthenticData && dnsMessage.IsDnsSecOk;
+		}
+
+		public static bool IsSupportDnsSec(DomainName domain)
+		{
+			var dnsClient = new DnsClient(IPAddress.Parse(@"1.1.1.1"), 10000);
+
+			var options = new DnsQueryOptions
+			{
+				IsRecursionDesired = true,
+				IsCheckingDisabled = false,
+				IsEDnsEnabled = true,
+				IsDnsSecOk = true,
+			};
+
+			var dnsMessage = dnsClient.Resolve(domain, RecordType.A, RecordClass.INet, options);
+
+			if (dnsMessage == null || dnsMessage.ReturnCode != ReturnCode.NoError && dnsMessage.ReturnCode != ReturnCode.NxDomain)
+			{
+				return false;
+			}
+
+			return dnsMessage.IsAuthenticData && dnsMessage.IsDnsSecOk;
+		}
+
+		public static bool IsSupportEcs(IPEndPoint dns)
+		{
+			var dnsClient = new DnsClient(dns.Address, 10000, dns.Port);
+			var ecs = new ClientSubnetOption(32, IPAddress.Parse(@"202.96.199.133"));
+
+			var options = new DnsQueryOptions
+			{
+				IsEDnsEnabled = true,
+				IsRecursionDesired = true,
+				EDnsOptions = new OptRecord { Options = { ecs } }
+			};
+
+			var dnsMessage = dnsClient.Resolve(DomainName.Parse(@"www.bing.com"), RecordType.A, RecordClass.INet, options);
+
+			if (dnsMessage == null || dnsMessage.ReturnCode != ReturnCode.NoError && dnsMessage.ReturnCode != ReturnCode.NxDomain)
+			{
+				return false;
+			}
+
+			if (dnsMessage.EDnsOptions.Options.Count > 0)
+			{
+				if (dnsMessage.EDnsOptions.Options[0] is ClientSubnetOption a)
+				{
+					return a.Type == EDnsOptionType.ClientSubnet
+						&& Equals(a.Address, ecs.Address)
+						&& a.Family == ecs.Family
+						&& a.SourceNetmask == ecs.SourceNetmask;
+				}
+			}
+
+			return false;
+		}
+	}
+}
---
+++ UnitTest/DnsValidationTest.cs
@@ -0,0 +1,41 @@
+﻿using ARSoft.Tools.Net;
+using DNSInfo.Utils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+
+namespace UnitTest
+{
+	[TestClass]
+	public class DnsValidationTest
+	{
+		private readonly IPEndPoint googleDns = new IPEndPoint(IPAddress.Parse(@"8.8.8.8"), 53);
+		private readonly IPEndPoint cloudflareDns = new IPEndPoint(IPAddress.Parse(@"1.1.1.1"), 53);
+		private readonly IPEndPoint dnspod = new IPEndPoint(IPAddress.Parse(@"119.29.29.29"), 53);
+
+		[TestMethod]
+		public void DnsServerDnsSecTest()
+		{
+			Assert.IsTrue(DnsValidation.IsSupportDnsSec(cloudflareDns));
+			Assert.IsFalse(DnsValidation.IsSupportDnsSec(dnspod));
+		}
+
+		[TestMethod]
+		public void DomainDnsSecTest()
+		{
+			var supportedDomain1 = DomainName.Parse(@"pir.org");
+			var supportedDomain2 = DomainName.Parse(@"www.isoc.org");
+			var unsupportedDomain = DomainName.Parse(@"www.google.com");
+			Assert.IsTrue(DnsValidation.IsSupportDnsSec(supportedDomain1));
+			Assert.IsTrue(DnsValidation.IsSupportDnsSec(supportedDomain2));
+			Assert.IsFalse(DnsValidation.IsSupportDnsSec(unsupportedDomain));
+		}
+
+		[TestMethod]
+		public void EcsTest()
+		{
+			Assert.IsTrue(DnsValidation.IsSupportEcs(googleDns));
+			Assert.IsFalse(DnsValidation.IsSupportEcs(cloudflareDns));
+			Assert.IsTrue(DnsValidation.IsSupportEcs(dnspod));
+		}
+	}
+}
